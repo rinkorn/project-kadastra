b76f06f76b0f86ef72e3e5c7f7b9e27dcfa5545c
AlbM <rinkorn.alb@gmail.com>
2026-04-25 04:30:42 +0300

    feat: implement BuildSyntheticTarget use case

--- src/kadastra/usecases/build_synthetic_target.py
+++ src/kadastra/usecases/build_synthetic_target.py
@@ -1,3 +1,4 @@
+from kadastra.etl.synthetic_target import compute_synthetic_target
 from kadastra.ports.gold_feature_reader import GoldFeatureReaderPort
 from kadastra.ports.gold_feature_store import GoldFeatureStorePort
 
@@ -9,7 +10,12 @@ class BuildSyntheticTarget:
         target_store: GoldFeatureStorePort,
         seed: int,
     ) -> None:
-        raise NotImplementedError
+        self._gold_reader = gold_reader
+        self._target_store = target_store
+        self._seed = seed
 
     def execute(self, region_code: str, resolutions: list[int]) -> None:
-        raise NotImplementedError
+        for resolution in resolutions:
+            gold = self._gold_reader.load(region_code, resolution)
+            df = compute_synthetic_target(gold, seed=self._seed)
+            self._target_store.save(region_code, resolution, df)
