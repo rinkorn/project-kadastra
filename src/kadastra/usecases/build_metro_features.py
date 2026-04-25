9e4edeadb063198a5b6b9812148f9902a7f82d84
AlbM <rinkorn.alb@gmail.com>
2026-04-25 03:52:19 +0300

    feat: implement BuildMetroFeatures use case

--- src/kadastra/usecases/build_metro_features.py
+++ src/kadastra/usecases/build_metro_features.py
@@ -1,3 +1,8 @@
+import io
+
+import polars as pl
+
+from kadastra.etl.metro_features import compute_metro_features
 from kadastra.ports.coverage_reader import CoverageReaderPort
 from kadastra.ports.feature_store import FeatureStorePort
 from kadastra.ports.raw_data import RawDataPort
@@ -19,4 +24,10 @@ class BuildMetroFeatures:
         self._entrances_key = entrances_key
 
     def execute(self, region_code: str, resolutions: list[int]) -> None:
-        raise NotImplementedError
+        stations = pl.read_csv(io.BytesIO(self._raw_data.read_bytes(self._stations_key)))
+        entrances = pl.read_csv(io.BytesIO(self._raw_data.read_bytes(self._entrances_key)))
+
+        for resolution in resolutions:
+            coverage = self._coverage_reader.load(region_code, resolution)
+            features = compute_metro_features(coverage, stations, entrances)
+            self._feature_store.save(region_code, resolution, "metro", features)
