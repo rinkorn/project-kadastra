6e084ef821b111ff2e2ada9bb8e96da78c60973b
AlbM <rinkorn.alb@gmail.com>
2026-04-25 03:57:36 +0300

    feat: implement BuildBuildingsFeatures use case

--- src/kadastra/usecases/build_buildings_features.py
+++ src/kadastra/usecases/build_buildings_features.py
@@ -1,6 +1,22 @@
+import io
+
+import polars as pl
+
+from kadastra.etl.building_features import compute_building_features
 from kadastra.ports.coverage_reader import CoverageReaderPort
 from kadastra.ports.feature_store import FeatureStorePort
 from kadastra.ports.raw_data import RawDataPort
+
+# OSM building CSVs have ambiguous columns: housenumber mixes digits and letters,
+# levels may be "2-3", start_date may be a year or a full date. Read everything
+# potentially-mixed as Utf8; compute_building_features casts levels/flats with strict=False.
+_BUILDINGS_SCHEMA_OVERRIDES = {
+    "levels": pl.Utf8,
+    "flats": pl.Utf8,
+    "housenumber": pl.Utf8,
+    "postcode": pl.Utf8,
+    "start_date": pl.Utf8,
+}
 
 
 class BuildBuildingsFeatures:
@@ -17,4 +33,12 @@ class BuildBuildingsFeatures:
         self._buildings_key = buildings_key
 
     def execute(self, region_code: str, resolutions: list[int]) -> None:
-        raise NotImplementedError
+        buildings = pl.read_csv(
+            io.BytesIO(self._raw_data.read_bytes(self._buildings_key)),
+            schema_overrides=_BUILDINGS_SCHEMA_OVERRIDES,
+        )
+
+        for resolution in resolutions:
+            coverage = self._coverage_reader.load(region_code, resolution)
+            features = compute_building_features(coverage, buildings)
+            self._feature_store.save(region_code, resolution, "buildings", features)
