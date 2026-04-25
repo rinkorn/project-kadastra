674fe2369d7c490dfbdc73826f03762f3bc2df64
AlbM <rinkorn.alb@gmail.com>
2026-04-25 03:15:01 +0300

    feat: implement BuildRegionCoverage use case

--- src/kadastra/usecases/build_region_coverage.py
+++ src/kadastra/usecases/build_region_coverage.py
@@ -1,3 +1,4 @@
+from kadastra.etl.h3_coverage import geometry_to_h3_cells
 from kadastra.ports.coverage_store import CoverageStorePort
 from kadastra.ports.region_boundary import RegionBoundaryPort
 
@@ -8,4 +9,10 @@ class BuildRegionCoverage:
         self._store = store
 
     def execute(self, region_code: str, resolutions: list[int]) -> None:
-        raise NotImplementedError
+        geometry = self._boundary.get_boundary(region_code)
+        cells: list[tuple[str, int]] = [
+            (cell, resolution)
+            for resolution in resolutions
+            for cell in geometry_to_h3_cells(geometry, resolution)
+        ]
+        self._store.save(region_code, cells)
